5627144752aeacb81a2005b77ef89159d68d2f76
AlexLundin <aml140830@utdallas.edu>
2019-04-15 21:17:58 -0500

    settings.py host fix and webapp view
    fixed settings.py host pattern by removing "http://" before the host name. Also added /webapp view

--- LoyaltyCrawler/webapp/urls.py
+++ LoyaltyCrawler/webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.webapp, name='webapp'),
 ]
 
 # As a final step, create a file inside your catalog folder called urls.py,
--- LoyaltyCrawler/webapp/views.py
+++ LoyaltyCrawler/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def webapp(request):
+    return HttpResponse("Whattup mothasucka. You've reached the webapp view.")
